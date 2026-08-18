8de83c0b666155629368f9ffc629ed7febbd3b38
khethiwe.ngema04 <khethiwe.ngema04@gmail.com>
2026-08-18 15:37:45 +0200

    created the store file and stored log in slice

--- src/store.ts
+++ src/store.ts
@@ -1,1 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from '../src/Features/login'
+
+export default configureStore({
+	reducer: {
+		login: loginReducer,
+	},
+})
+
+// export type RootState = ReturnType<typeof store.getState>
+// export type AppDispatch = typeof store.dispatch

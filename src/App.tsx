b4e442d5eaef4f6c3babed8ad462f3cf4742c1c9
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-08-20 00:27:51 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,19 @@ import { Log } from './Components/Pages/LogIn/Log'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './Components/Pages/Home/Home'
 import Register from './Components/Pages/Registering/Registering'
-// import { Profile } from './Components/Pages/Profile'
+import Profile from './Components/Pages/Profile/Profile'
 
 function App() {
     return (
         <>
+
             <Routes>
                 <Route path="/" element={<Log />} />
-                <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/profilepage" element={<Profile />} />
             </Routes>
 
-
-
-            {/* <Routes> */}
-            {/* <Route path="/" element={<Log />} /> */}
-            {/* <Route path="/profilepage" element={<Profile />} /> */}
-            {/* </Routes> */}
 
         </>
 

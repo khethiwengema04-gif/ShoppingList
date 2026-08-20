9e05d5b71ca275a9220455bc698b4ce6604ff6b0
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-08-20 15:22:00 +0200

    created the interfaces & the states for register

--- src/Features/register.ts
+++ src/Features/register.ts
@@ -1,30 +1,57 @@
 import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface RegisterState {
+    fullnames: string,
+    lastname: string,
+    emailadress: string,
+    cellphone: string,
+    password: string,
+    verifypassword: string
+}
+
+const initialState: RegisterState = {
+    fullnames: '',
+    lastname: '',
+    emailadress: '',
+    cellphone: '',
+    password: '',
+    verifypassword: '',
+}
+
+//thunk
 
 export const registerSlice = createSlice({
     name: 'register',
-    initialState: {
-        fullnames: '',
-        lastname: '',
-        emailadress: '',
-        cellphone: '',
-        password: '',
-        verifypassword: ''
-        
-        
-    },
+    initialState,
 
     reducers: {
-        getUser: state =>{
-            state. fullnames= "khethiwe amanda"
-            state.lastname= "ngema"
-            state.emailadress="khethiwe@gmail.com"
-            state.password= "khethy"
-            state.verifypassword= "khethy"
-           
-        }
-    }
+
+        setfullNames: (state, action: PayloadAction<string>) => {
+            state.fullnames = action.payload
+        },
+        setLastName: (state, action: PayloadAction<string>) => {
+            state.lastname = action.payload
+        },
+        setemailadress: (state, action: PayloadAction<string>) => {
+            state.emailadress = action.payload
+        },
+        setcellphone: (state, action: PayloadAction<string>) => {
+            state.cellphone = action.payload
+        },
+
+        setpassword: (state, action: PayloadAction<string>) => {
+            state.password = action.payload
+        },
+
+        setverifypassword: (state, action: PayloadAction<string>) => {
+            state.verifypassword = action.payload
+        },
+
+    },
+
 
 })
 
-export const { getUser } =registerSlice.actions
+export const { setfullNames, setLastName, setemailadress, setcellphone, setpassword, setverifypassword } = registerSlice.actions
 export default registerSlice.reducer

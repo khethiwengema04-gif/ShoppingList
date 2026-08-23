561b7ab85967720abe63cd8755e6b966a425b22e
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-08-23 23:25:45 +0200

    changed the background colors

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../Navbar/Navbar'
-// import styles from './Home.module.css'
+import styles from './Home.module.css'
 import { Search } from '../Search/Search'
 
 export const Home = () => {

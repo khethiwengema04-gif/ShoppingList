9ae1b9908b868f878d9e8945dc9d4f9618f1873a
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-08-24 11:04:52 +0200

    creating the categories on my home page

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -2,12 +2,35 @@ import React from 'react'
 import Navbar from '../../Navbar/Navbar'
 import styles from './Home.module.css'
 import { Search } from '../Search/Search'
+// import type { CategoryList } from '../Home/
 
-export const Home = () => {
+
+interface CategoryListProps {
+
+    onDelete: (id: number) => void
+    onEdit: (id: number) => void
+}
+
+
+
+export const Home: React.FC<CategoryListProps> = ({ }) => {
     return (
         <div>
             <Navbar />
             <Search />
+            <div>
+                <h1 className={styles.tittle}>CATEGORIES:</h1>
+
+
+                <button className={styles.button} onClick={() => { }}>
+                    Delete
+                </button>
+
+                <button className={styles.button} onClick={() => { }} >
+                    Edit
+                </button>
+            </div>
         </div>
     )
+
 }
--- src/Components/Pages/Profile/Profile.tsx
+++ src/Components/Pages/Profile/Profile.tsx
@@ -2,7 +2,30 @@ import React from 'react'
 import styles from './Profile.module.css'
 import profile from '../../../Assets/profile.png'
 import { useNavigate } from 'react-router-dom'
+// import { useState } from 'react'
 
+
+// type ProfileProps = {
+//     onEdit: (Profile: Profile) => void
+//     editProfile?: Profile | null
+// }
+
+// export const Profile: React.FC<ProfileProps> = ({ onEdit, editProfile }) => {
+
+//     const [email, setemail] = useState(editProfile?.email ?? '')
+//     const [fullnames, setfullnames] = useState(editProfile?.fullnames ?? '')
+//     const [lastname, setlastname] = useState(editProfile?.lastname ?? '')
+//     const [cellphone, setcellphone] = useState(editProfile?.cellphone ?? '')
+
+//     const handleSubmit = () => {
+//         if (editProfile) {
+//             onEdit({ ...email, fullnames, lastname, cellphone })
+//         }
+
+// const navigate = useNavigate()
+// const handleEditProfile = () => {
+//     navigate('/')
+// }
 
 const Profile = () => {
 
@@ -10,13 +33,15 @@ const Profile = () => {
     const handleBackToHomeClick = () => {
         navigate('/')
     }
+
+
     return (
         <div className={styles.container}>
             <div className={styles.loginForm}>
                 <h1 className={styles.tittle}>MY PROFILE</h1>
                 <img src={profile} alt='profile' className={styles.profileImg} />
 
-                <div className={styles.email}>
+                <div className={styles.email}>09
                     <label>Khethy@gmail.com</label>
                 </div>
 
@@ -31,9 +56,15 @@ const Profile = () => {
 
 
             </div>
+
+            {/* <div>
+                <button onClick={handleEditProfile}>EditProfile</button>
+            </div> */}
+
             <div>
                 <button onClick={handleBackToHomeClick}>Back To Home</button>
             </div>
+
 
 
         </div>

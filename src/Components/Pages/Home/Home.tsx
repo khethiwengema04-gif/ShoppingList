c5233baa17323d262845caea8337434df1b44cb6
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-08-24 11:19:56 +0200

    updated the category component and removed the functions from home

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -2,35 +2,13 @@ import React from 'react'
 import Navbar from '../../Navbar/Navbar'
 import styles from './Home.module.css'
 import { Search } from '../Search/Search'
-// import type { CategoryList } from '../Home/
 
-
-interface CategoryListProps {
-
-    onDelete: (id: number) => void
-    onEdit: (id: number) => void
-}
-
-
-
-export const Home: React.FC<CategoryListProps> = ({ }) => {
+export const Home = () => {
     return (
         <div>
             <Navbar />
             <Search />
-            <div>
-                <h1 className={styles.tittle}>CATEGORIES:</h1>
 
-
-                <button className={styles.button} onClick={() => { }}>
-                    Delete
-                </button>
-
-                <button className={styles.button} onClick={() => { }} >
-                    Edit
-                </button>
-            </div>
         </div>
     )
-
 }

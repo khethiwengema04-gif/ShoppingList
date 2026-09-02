9903e35d1fac50242a24c4a6bcdacf34f5481c2f
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-09-02 06:35:39 +0200

    Added the edit thunk

--- src/Features/List.ts
+++ src/Features/List.ts
@@ -14,6 +14,7 @@ interface ItemlistState extends ItemList {
     name: string;
     quantity: number;
     optionalNote: string;
+    editingItemId: string | null;
     isLoading: boolean;
     error: string | null;
 }
@@ -23,6 +24,7 @@ const initialState: ItemlistState = {
     name: "",
     quantity: 0,
     optionalNote: "",
+    editingItemId: null,
     isLoading: false,
     error: null,
 }
@@ -74,6 +76,45 @@ export const getItemListThunk = createAsyncThunk(
 );
 
 
+//DELETE THUNK
+export const deleteItemList = createAsyncThunk(
+    "ItemList/deleteItemList",
+    async (id: string, { rejectWithValue }) => {
+        try {
+            const response = await fetch(`http://localhost:3000/items/${id}`, {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+            });
+            if (!response.ok) throw new Error("Failed to delete Item");
+            return id;
+        } catch (error) {
+            return rejectWithValue((error as Error).message);
+        }
+    },
+);
+
+//EDIT THUNK
+export const editList = createAsyncThunk(
+    "List/editList",
+    async (editList: ItemList, { rejectWithValue }) => {
+        try {
+            const response = await fetch(
+                `http://localhost:3000/items/${editList.id}`,
+                {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(editList),
+                },
+            );
+            if (!response.ok) throw new Error("Failed to update item");
+            return (await response.json()) as ItemList;
+        } catch (error) {
+            return rejectWithValue((error as Error).message);
+        }
+    },
+);
+
+
 
 
 export const ItemListSlice = createSlice({
@@ -92,6 +133,19 @@ export const ItemListSlice = createSlice({
         AddItemList: (state, action: PayloadAction<ItemList[]>) => {
             state.itemList = action.payload;
         },
+        setEditingItem: (state, action: PayloadAction<ItemList>) => {
+            state.editingItemId = action.payload.id || null;
+            state.name = action.payload.name
+            state.quantity = action.payload.quantity
+            state.optionalNote = action.payload.optionalNote;
+        },
+        clearEditingItem: (state) => {
+            state.editingItemId = null;
+            state.name = '';
+            state.quantity = 0;
+            state.optionalNote = '';
+        }
+
     },
 
     extraReducers: (builder) => {
@@ -129,9 +183,39 @@ export const ItemListSlice = createSlice({
                 state.error = action.error.message || 'Failed to load items'
             })
 
+            .addCase(deleteItemList.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(deleteItemList.fulfilled, (state, action: PayloadAction<string>) => {
+                state.isLoading = false;
+                state.itemList = state.itemList.filter((list) => list.id !== action.payload);
+            })
+            .addCase(deleteItemList.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload as string || 'Failed to delete item';
+            })
+
+            .addCase(editList.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(editList.fulfilled, (state, action: PayloadAction<ItemList>) => {
+                state.isLoading = false;
+                state.itemList = state.itemList.map((item) =>
+                    item.id === action.payload.id ? action.payload : item,
+                );
+                state.editingItemId = null;
+                state.name = '';
+                state.quantity = 0;
+                state.optionalNote = '';
+            })
+            .addCase(editList.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload as string;
+            });
     }
 
 })
 
-export const { AddName, AddQuantity, AddOptionalnote, AddItemList } = ItemListSlice.actions
+export const { AddName, AddQuantity, AddOptionalnote, AddItemList, setEditingItem, clearEditingItem } = ItemListSlice.actions
 export default ItemListSlice.reducer

0f9a11a2f6b9f5abca16414befb9ce2d1317aa00
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-09-02 06:32:17 +0200

    Working with the edit functionality

--- src/Components/ItemCard/ItemCard.tsx
+++ src/Components/ItemCard/ItemCard.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import style from './ItemCard.module.css'
 import { useNavigate } from 'react-router-dom'
+import type { ItemList } from '../../Features/List'
+
 
 interface ItemCardProps {
+    itemlist: ItemList;
+    onEdit: () => void;
+    onDelete: () => void;
 
 }
 
-const ItemCard = () => {
+export const ItemCard = ({ itemlist, onDelete, onEdit }: ItemCardProps) => {
+
+
     return (
-        <div>ItemCard</div>
+        <div className={style.itemlistCard}>
+            <div className={style.itemlistButton}>
+                <h2>{itemlist.name}</h2>
+                <h2>{itemlist.quantity}</h2>
+                <h2>{itemlist.optionalNote}</h2>
+
+                <button onClick={onEdit} className={style.editButton}  >
+                    Edit
+                </button>
+
+                <button onClick={onDelete} className={style.deletebutton}  >
+                    Delete
+                </button>
+
+
+            </div>
+
+        </div>
     )
+
 }
 
 export default ItemCard

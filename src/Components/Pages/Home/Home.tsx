96df08a8beb2fa7c33be4e8a46db4c15c6e1bf65
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-09-02 06:33:01 +0200

    mapping

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -5,10 +5,14 @@ import CategoryList from '../../CategoryList/CategoryList'
 import { Card } from '../../Card/Card'
 import { useSelector } from 'react-redux'
 import type { RootState } from '../../../store'
+import type { AppDispatch } from '../../../store'
+import { useDispatch } from 'react-redux'
+import { deleteCategory } from '../../../Features/category'
 
 
 export const Home = () => {
     const category = useSelector((state: RootState) => state.category)
+    const dispatch = useDispatch<AppDispatch>();
 
     return (
         <div>
@@ -18,9 +22,12 @@ export const Home = () => {
             <div>
                 {
                     category.category.map((link) => (
-                        <Card key={link.id} category={link}
+                        <Card key={link.id}
+                            category={link}
                             onView={() => ({})}
-                            onDelete={() => ({})} />
+                            onDelete={() => {
+                                if (link.id) dispatch(deleteCategory(link.id))
+                            }} />
                     ))
                 }
             </div>

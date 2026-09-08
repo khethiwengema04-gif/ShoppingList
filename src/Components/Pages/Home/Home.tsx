bba5783b0ab19e1b3de65eca2ed4cfac53247c04
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-09-08 14:04:36 +0200

    Mapping

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -7,13 +7,30 @@ import { useSelector } from 'react-redux'
 import type { RootState } from '../../../store'
 import type { AppDispatch } from '../../../store'
 import { useDispatch } from 'react-redux'
-import { deleteCategory } from '../../../Features/category'
+import { deleteCategory, getCategory } from '../../../Features/category'
 import { editCategory } from '../../../Features/category'
+import { useEffect } from 'react'
 
 
 export const Home = () => {
     const category = useSelector((state: RootState) => state.category)
     const dispatch = useDispatch<AppDispatch>();
+    const userId = useSelector((state: RootState) => state.login.user?.id ? String(state.login.user.id) : "");
+
+    let User = useSelector((state: RootState) => state.login.user);
+    if (!User) {
+        const savedUser = localStorage.getItem('user');
+        if (savedUser) User = JSON.parse(savedUser);
+    }
+
+    //    const userId = User?.id ? String(User.id) : "";
+
+    useEffect(() => {
+        if (userId.trim() !== '') {
+            dispatch(getCategory(userId));
+        }
+    }, [dispatch, userId]);
+
     const handleSearch = (query: string) => {
         console.log("User searched for:", query);
     };
